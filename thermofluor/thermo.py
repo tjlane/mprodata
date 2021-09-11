532269005ce9df2dd13078a8e2cba1648b991ede
tjlane <thomas.joseph.lane@gmail.com>
2021-09-11 19:39:17 +0200

    cleaned up TF plots, made plots of raw traces

--- thermofluor/thermo.py
+++ thermofluor/thermo.py
@@ -9,34 +9,37 @@ def load_raw(file_path, cycles=620):
     record = False
     results = np.zeros([cycles, 96])
     
+    i = 0
+
     with open(file_path, 'r') as f:
         for line in f:
 
             if line.startswith('[Raw Data]'):
+                print('--> reading')
                 record = True
 
             elif line.startswith('Well'):
-                # first line after [Raw Data]
-                # Well    Well Position   Cycle   x4-m4
                 pass
 
             elif line.strip() == '':
                 record = False
+                print('<-- done, %d lines' % i)
                 #break
 
             elif record:
                 d = line.split()
+                print(d)
                 
                 well   = int(d[0]) - 1
                 cycle  = int(d[2]) - 1
                 fluoro = float(d[3].replace(',', ''))
                 
                 results[cycle, well] = fluoro
-            
+
     return results
 
 
-def load_melt(file_path):
+def load_melt(file_path, mode='derivative'):
     
     # Well    Well Position   Reading Temperature     Fluorescence    Derivative      Target Name
     
@@ -66,8 +69,14 @@ def load_melt(file_path):
                 derv = float(l[5].replace(',', ''))
                 
                 if name not in series.keys():
-                    series[name] = []    
-                series[name].append(derv)
+                    series[name] = []
+
+                if mode == 'derivative':
+                    series[name].append(derv)
+                elif mode == 'raw':
+                    series[name].append(fluo)
+                else:
+                    raise ValueError('invalid mode: %s' % mode)
 
                 if len(series.keys()) <= 2: # the first time through
                     series['temperature'].append(temp)
